542134ec2d34ab8da91691e42b546c06e9443fd9
Sylvia Dulaney <SPinegar07@ufl.edu>
2015-09-22 21:48:47 -0500

    created object
    created new array with most recent five presidents and then used console.log to pull out third president in the list

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ var iceCream = [
 	];
 
 iceCream.push("Hazed and Confused");
+
+var presidents [
+	{firstName:"Barack", lastName:"Obama", termLength:"8 years", party:"Democrat", yearsofPresidency:"7"},
+	{firstName:"George W.", lastName:"Bush", termLength:"8 years", party:"Republican", yearsofPresidency:"8"},
+	{firstName:"Bill", lastName:"Clinton", termLength:"8 years", party:"Democrat", yearsofPresidency:"8"},
+	{firstName:"George H. W.", lastName:"Bush", termLength:"4 years", party:"Republican", yearsofPresidency:"4"},
+	{firstName:"Ronald", lastName:"Reagan", termLength:"8 years", party:"Republican", yearsofPresidency:"8"},
+	]
+
+presidents[2].lastName.firstName
